839591633d97ff88cfca80dd6f79df2a5af63523
Jennifer <jennifer.maldonado@intraway.com>
2019-06-10 15:22:34 -0300

    ANG-72

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
+// ViewEncapsulation.None by default is emulated
+// native use shadow DOM always
+// emulated use only when is execute in a browser that support  this features
+
 export class ServerElementComponent implements OnInit {
 
   // Decorator in class level or in properties level
