d0719c9beb3c1d768dbc114a6adacbc5918a9a58
Jennifer <jennifer.maldonado@intraway.com>
2019-06-10 18:36:25 -0300

    ANG-75

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, Output, EventEmitter, ViewChild, ElementRef} from '@angular/core';
 
-let viewChild = ViewChild('serverContentInput');
+
 
 @Component({
   selector: 'app-cockpit',
@@ -13,15 +13,15 @@ export class CockpitComponent implements OnInit {
   // newServerName = '';
   // newServerContent = '';
 
-  @viewChild serverContentInput: ElementRef;
-  // @ViewChild(Cock) serverContentInput;
-
-  // CUSTOM EVENTS
-  // with our own event in the html element
+  // CUSTOM EVENTS - with our own event in the html element
   // make you own event
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   // Output let you pass a variable from child component to father component
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+
+
+  // @ViewChild(Cock) serverContentInput;
+  @ViewChild('serverContentInput') serverContentInput: ElementRef; // the teacher dont recommend this way for access to dom html element
 
   constructor() {
   }
@@ -32,14 +32,14 @@ export class CockpitComponent implements OnInit {
   // with the event click
   // onAddServer(serverData: {serverName: string, serverContent: string}) {
   onAddServer(nameInput: HTMLInputElement) {
-      console.log(this.serverContentInput); // this is an element Ref
+      console.log(this.serverContentInput.nativeElement); // this is an element Ref
 
       // EXAMPLE LOCAL REFERENCE
       // console.log(nameInput.value); // the element with all his properties.
-      // this.serverCreated.emit({
-      //   serverName: nameInput.value, // local reference
-      //   serverContent: this.newServerContent
-      // });
+      this.serverCreated.emit({
+        serverName: nameInput.value, // local reference
+        serverContent:this.serverContentInput.nativeElement.value
+      });
 
     // EXAMPLE decorator input/output
     // this.serverElements.push({
@@ -49,13 +49,15 @@ export class CockpitComponent implements OnInit {
     // });
   }
 
-  onAddBluePrint(blueprintData: {serverName: string, serverContent: string}) {
+  // with template variables
+  // onAddBluePrint(blueprintData: {serverName: string, serverContent: string}) {
+  onAddBluePrint(nameInput: HTMLInputElement) {
 
-    // local reference
-    // this.blueprintCreated.emit({
-  //       serverName: this.newServerName,
-  //       serverContent: this.newServerContent
-  // });
+    // local reference passed by Val, and passed local reference between templates
+    this.blueprintCreated.emit({
+        serverName: nameInput.value,
+        serverContent: this.serverContentInput.nativeElement.value
+    });
 
     // input/output example
     // this.serverElements.push({
