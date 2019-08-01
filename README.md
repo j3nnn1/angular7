caf228c76f6316425a70205f7388a630eb5c6eac
j3nnn1 <jcmm986@gmail.com>
2019-08-01 01:37:28 +0000

    ANG-85

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # angular7
+
+usar nvm
+```
+nvm use 10.15.3
+```
+mover a carpeta del proyecto 
+
+```
+cd ang7-clean
+```
+iniciar servidor
+
+```
+ng  serve
+```
--- ang7-cleanup/src/app/recipes/recipes-item/recipes-item.component.ts
+++ ang7-cleanup/src/app/recipes/recipes-item/recipes-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipe.model';
+/* /home/j3nnn1/git/angular7/ang7-cleanup/src/app/recipes/recipe.model.ts
+/home/j3nnn1/git/angular7/ang7-cleanup/src/app/recipes/recipes-item/recipes-item.component.ts */
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-item.component.css']
 })
 export class RecipesItemComponent implements OnInit {
-
+  /* add data from outside add decorator INPUT*/
+  /* this refer from recipes-lists.html watch for recipeEl*/
+  // passing data from list to item from one component to another with property binding
+  // this is the receiver
+  @Input() recipe: Recipe;
   constructor() { }
 
   ngOnInit() {
