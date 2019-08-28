4f9602b8b8c0c59da840d271efbd55c98abbc730
j3nnn1 <jcmm986@gmail.com>
2019-08-28 02:39:02 +0000

    ANG-148

---
+++ routing-start/src/app/auth.service.ts
@@ -0,0 +1,20 @@
+export class AuthService {
+    loggedIn = false;
+
+    login() {
+      this.loggedIn = true;
+    }
+    logout() {
+      this.loggedIn = false;
+    }
+    isAuthenticate() {
+      const promise = new Promise(
+        (resolve, reject ) => {
+          setTimeout(() => {
+            resolve(this.loggedIn);
+          }, 800);
+        }
+      );
+      return promise;
+    }
+}
