29b33de58bc497ce3fb0e0aecbfe6514a0b42706
jennifer.maldonado <jennifer.maldonado@intraway.com>
2019-09-05 23:53:58 -0300

    ANG-140

--- cat-card/cat-cards/src/app/app-routing.module.ts
+++ cat-card/cat-cards/src/app/app-routing.module.ts
@@ -4,18 +4,23 @@ import {CatComponent} from './cat/cat.component';
 import {CatAddComponent} from './cat-add/cat-add.component';
 import {CatDetailsComponent} from './cat-details/cat-details.component';
 import {NotFoundComponent} from './not-found/not-found.component';
+import {AppComponent} from './app.component';
+import {CardComponent} from './card/card.component';
 
 
 const routes: Routes = [
-  { path: '', component: CatComponent}, // could be redirectTo: Component, pathMatch: full
-  { path: 'add', component: CatAddComponent},
-  { path: 'details', component: CatDetailsComponent},
+  { path: '', redirectTo: '/cat', pathMatch: 'full'}, // could be redirectTo: Component, pathMatch: full
+  { path: 'cat', component: CardComponent, children : [
+      { path: '', component: CatComponent, pathMatch: 'full' },
+      { path: 'add', component: CatAddComponent, pathMatch: 'full'},
+      { path: ':id/details', component: CatDetailsComponent, pathMatch: 'full'}
+    ]},
   { path: 'not-found', component: NotFoundComponent},
   { path: '**', redirectTo: '/not-found'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- cat-card/cat-cards/src/app/cat-add/cat-add.component.ts
+++ cat-card/cat-cards/src/app/cat-add/cat-add.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatAddComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+    console.log('desde la clase add cat');
   }
 
 }
